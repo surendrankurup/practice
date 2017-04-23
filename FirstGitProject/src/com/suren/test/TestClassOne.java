2c19f1d6f7194a413fa05d932456363faf947b95
surendrankurup <surendrankurup@jyothys-mbp.t-mobile.com>
2017-04-23 07:59:19 -0700

    suren changes

--- FirstGitProject/src/com/suren/test/TestClassOne.java
+++ FirstGitProject/src/com/suren/test/TestClassOne.java
@@ -4,7 +4,7 @@ public class TestClassOne {
 
 	public static void main(String[] args) {
 		System.out.println("Test Class One");
-		System.out.println("Test Class two modified");
+		System.out.println("Test Class two modified suren");
 		
 	}
 
